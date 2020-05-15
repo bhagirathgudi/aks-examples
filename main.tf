85427c914b37e4319e9615b3537537b88da80e66
Bhagirath <bhagirath009@hotmail.com>
2020-05-15 01:55:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
     version = "~>2.0"
-    subscription_id = "${var.sub}"
-    client_id       = "${var.client_id}"
-    client_secret   = "${var.client_secret}"
-    tenant_id       = "${var.tenant_id}"
     features {}
 }
 
